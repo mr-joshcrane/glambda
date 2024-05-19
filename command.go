6e14b67696d89875a20d58c254926e932de884fe
Joshua Crane <josh.crane512@gmail.com>
2024-05-19 14:24:24 +0800

    Adding in command structure

--- command.go
+++ command.go
@@ -1,27 +1,58 @@
 package glambda
 
 import (
+	"fmt"
+	"io"
+
 	"github.com/spf13/cobra"
 )
 
-func Main(args []string) error {
-	var rootCmd = &cobra.Command{Use: "glambda"}
-	commands := []func([]string) *cobra.Command{
-		DeployCommand,
-	}
+type CommandOptions func(*cobra.Command) error
 
-	for _, cmd := range commands {
-		rootCmd.AddCommand(cmd(args))
+func WithOutput(w io.Writer) CommandOptions {
+	return func(cmd *cobra.Command) error {
+		cmd.SetOutput(w)
+		return nil
 	}
+}
+
+func Main(args []string, opts ...CommandOptions) error {
+	var rootCmd = &cobra.Command{
+		Use:   "glambda",
+		Short: "A tool for deploying Go binaries as AWS Lambda functions.",
+	}
+	rootCmd.SetArgs(args)
+	commands := []*cobra.Command{
+		DeployCommand(),
+	}
+	for _, opt := range opts {
+		err := opt(rootCmd)
+		if err != nil {
+			return err
+		}
+	}
+	rootCmd.AddCommand(commands...)
+	if len(args) == 0 {
+		rootCmd.Printf(rootCmd.UsageString())
+		return fmt.Errorf("no command provided")
+	}
+	rootCmd.SetArgs(args)
+	_, _, err := rootCmd.Find(args)
+	if err != nil {
+		rootCmd.Printf(rootCmd.UsageString())
+		return err
+	}
+	rootCmd.SetHelpCommand(&cobra.Command{Use: "no-help", Run: func(cmd *cobra.Command, args []string) {}})
+	rootCmd.CompletionOptions.DisableDefaultCmd = true
 	return rootCmd.Execute()
 }
 
-func DeployCommand(args []string) *cobra.Command {
+func DeployCommand() *cobra.Command {
 	var deployCmd = &cobra.Command{
-		Use:          "deploy [name] [source]",
-		Short:        "Package a Go binary and upload it as a lambda function.",
-		Args:         cobra.ExactArgs(2),
-		SilenceUsage: true,
+		Use:     "deploy functionName sourceCodePath",
+		Short:   "Package a Go binary and upload it as a lambda function.",
+		Args:    cobra.ExactArgs(2),
+		Example: `glambda deploy myFunctionName /path/to/sourceCode.go`,
 		RunE: func(cmd *cobra.Command, args []string) error {
 			functionName := args[0]
 			sourceCodePath := args[1]
--- command_test.go
+++ command_test.go
@@ -1,12 +1,43 @@
 package glambda_test
 
 import (
+	"bytes"
+	"strings"
 	"testing"
 
 	"github.com/mr-joshcrane/glambda"
 )
 
-func TestMain(t *testing.T) {
-	var args []string
-	glambda.Main(args)
+func TestMain_ReturnsErrorAndHelpOnInvalidArgs(t *testing.T) {
+	t.Parallel()
+	tc := []struct {
+		description string
+		args        []string
+	}{
+		{
+			description: "no args",
+			args:        []string{},
+		},
+		{
+			description: "partial command",
+			args:        []string{"deploy", "myFunctionName"},
+		},
+		{
+			description: "invalid args",
+			args:        []string{"invalid"},
+		},
+	}
+
+	for _, tt := range tc {
+		t.Run(tt.description, func(t *testing.T) {
+			buf := new(bytes.Buffer)
+			err := glambda.Main(tt.args, glambda.WithOutput(buf))
+			if err == nil {
+				t.Errorf("Expected error but got nil")
+			}
+			if !strings.Contains(buf.String(), "Usage:") {
+				t.Errorf("Expected help message but got: %s", buf.String())
+			}
+		})
+	}
 }
