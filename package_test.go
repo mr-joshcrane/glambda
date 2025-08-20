a4ece35db845ce758037fd6a0d894a0ec3681097
mr-joshcrane <57082182+mr-joshcrane@users.noreply.github.com>
2025-08-20 11:50:15 +0800

    reworking packaging (#17)
    Reworking how packaging works
    Updated tests to work better with temp dirs

--- package_test.go
+++ package_test.go
@@ -3,8 +3,12 @@
 package glambda_test
 
 import (
+	"archive/zip"
 	"bytes"
 	"fmt"
+	"io"
+	"os"
+	"path/filepath"
 	"testing"
 
 	"github.com/mr-joshcrane/glambda"
@@ -13,11 +17,14 @@ import (
 
 func TestPackage_PackagesLambdaFunction(t *testing.T) {
 	t.Parallel()
-	handler := "testdata/correct_test_handler/main.go"
+	handler := copyTestHandler(t)
 	buf := new(bytes.Buffer)
 	err := glambda.PackageTo(handler, buf)
 	if err != nil {
 		t.Error(err)
+	}
+	if len(buf.Bytes()) == 0 {
+		t.Fatal("expected non-empty zip file")
 	}
 	checkZipFile(t, buf.Bytes())
 }
@@ -29,7 +36,7 @@ func TestPrepareAction_CreateFunction(t *testing.T) {
 		FuncExists: false,
 		Err:        nil,
 	}
-	handler := "testdata/correct_test_handler/main.go"
+	handler := copyTestHandler(t)
 	l := glambda.Lambda{
 		Name:          "test",
 		HandlerPath:   handler,
@@ -52,7 +59,7 @@ func TestPrepareAction_UpdateFunction(t *testing.T) {
 		FuncExists: true,
 		Err:        nil,
 	}
-	handler := "testdata/correct_test_handler/main.go"
+	handler := copyTestHandler(t)
 	l := glambda.Lambda{
 		Name:          "test",
 		HandlerPath:   handler,
@@ -75,7 +82,7 @@ func TestPrepareAction_ErrorCase(t *testing.T) {
 		FuncExists: false,
 		Err:        fmt.Errorf("some client error"),
 	}
-	handler := "testdata/correct_test_handler/main.go"
+	handler := copyTestHandler(t)
 	l := glambda.Lambda{
 		Name:          "test",
 		HandlerPath:   handler,
@@ -85,4 +92,50 @@ func TestPrepareAction_ErrorCase(t *testing.T) {
 	if err == nil {
 		t.Fatal("expected error, got nil")
 	}
+}
+
+func checkZipFile(t *testing.T, zipContents []byte) {
+	t.Helper()
+	zipReader, err := zip.NewReader(bytes.NewReader(zipContents), int64(len(zipContents)))
+	if err != nil {
+		t.Errorf("failed to create zip reader, %v", err)
+	}
+	if len(zipReader.File) != 1 {
+		t.Errorf("expected 1 file in zip, got %d", len(zipReader.File))
+	}
+	file := zipReader.File[0]
+	if file.Name != "bootstrap" {
+		t.Errorf("expected file name to be bootstrap, got %s", zipReader.File[0].Name)
+	}
+	if file.Mode() != 0o755 {
+		t.Errorf("expected bootstrap file mode to be 0755, got %d", zipReader.File[0].Mode())
+	}
+	if file.UncompressedSize64 == 0 {
+		t.Errorf("expected bootstrap file to have content, got 0")
+	}
+
+}
+func copyTestHandler(t *testing.T) string {
+	tempDir := t.TempDir()
+	srcFile := "testdata/correct_test_handler/main.go"
+	dstFile := filepath.Join(tempDir, "main.go")
+
+	src, err := os.Open(srcFile)
+	if err != nil {
+		t.Fatalf("failed to open source file: %v", err)
+	}
+	defer src.Close()
+
+	dst, err := os.Create(dstFile)
+	if err != nil {
+		t.Fatalf("failed to create destination file: %v", err)
+	}
+	defer dst.Close()
+
+	_, err = io.Copy(dst, src)
+	if err != nil {
+		t.Fatalf("failed to copy file: %v", err)
+	}
+
+	return dstFile
 }
